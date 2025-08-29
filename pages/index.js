9dce1fb285cd8b18818c131a0015b2352ace2c59
grarande-admin <thegrahamr@gmail.com>
2025-08-29 11:32:14 -0600

    next.js TEST

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: 24, fontFamily: "system-ui, Arial"}}>
+      <h1>Welcome</h1>
+      <p>Static assets work from <code>/public</code> (e.g. <code>/images/foo.png</code>).</p>
+    </main>
+  );
+}
